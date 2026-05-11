87f24b340f7797cd6f1de081ba47e2599799d692
Pedro-IoT <pedromaiadepaiva@gmail.com>
2026-05-10 22:35:24 -0300

    creates tables and repositorys

---
+++ src/main/java/com/PedroMaia/auth_server/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.PedroMaia.auth_server.repository;
+
+import com.PedroMaia.auth_server.domain.User;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends ListCrudRepository<User, UUID> {
+
+    @Query("""
+        SELECT u.*,
+               r.id AS role_id,
+               r.name AS role_name
+        FROM "user" u
+        JOIN "role" r ON u.role_id = r.id
+        WHERE u.email = :email
+    """)
+    Optional<User> findByEmail(@Param("email") String email);
+
+    boolean existsByEmail(String email);
+}
